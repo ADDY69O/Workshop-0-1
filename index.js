796457f64e105fa59271db66be37753b024d5495
Aditya <kohsa9211420@gmail.com>
2024-01-19 11:39:06 +0530

    changes:

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log("Working on new-Features Branch with changes  --> 2")
 //this is the new features func
 function newFeatures(){
 
-    console.log("within the new features branch and with changes")
-    console.log("at line 2")
+    console.log("within the new features branch and with changes Done")
+    console.log("at line 2 & 4")
     function working(){
         console.log("working fine")
     }
@@ -12,5 +12,9 @@ function newFeatures(){
 }
 
 function main(){
-    console.log("here is the new main Function")
+
+    console.log("here is the new main Function added the changes here ::)")
+    function add(){
+        console.log("added the things here")
+    }
 }
