6d263b1f3e69f75979e270b68b047d4df879bc08
Aditya <kohsa9211420@gmail.com>
2024-01-19 11:29:07 +0530

    added new-features func

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-console.log("Working on new-Features Branch")
+console.log("Working on new-Features Branch with changes")
 
+//this is the new features func
 function newFeatures(){
 
     console.log("within the new features branch")
+    function working(){
+        console.log("working fine")
+    }
 }
