dfe5bf43b6492cf0b937a648ee9e622e5bc60f41
Aditya <kohsa9211420@gmail.com>
2024-01-19 11:31:03 +0530

    updated master branch

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-console.log("Working on new-Features Branch with changes")
+console.log("Working on new-Features Branch with changes  --> 2")
 
 //this is the new features func
 function newFeatures(){
 
-    console.log("within the new features branch")
+    console.log("within the new features branch and with changes")
+    console.log("at line 2")
     function working(){
         console.log("working fine")
     }
+    working();
 }
